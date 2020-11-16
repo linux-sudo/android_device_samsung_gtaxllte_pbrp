3c2686837926bec469db2643ad24d1644cafd3d1
TALU <taluemail272@gmail.com>
2020-11-16 20:24:04 +0000

    gtaxllte: update init.recovery.usb.rc for ffs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,6 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/13600000.usb/13600000.dwc3/gadget/lun%d/file"
 TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 127
